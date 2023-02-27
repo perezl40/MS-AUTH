fb1f7f91a335d221e74a63cff12e870eeb047efb
Jhon Perez <95254043+perezl40@users.noreply.github.com>
2023-02-27 16:38:43 -0500

    updated proto

--- src/domain/dtos/auth/auth.pb.ts
+++ src/domain/dtos/auth/auth.pb.ts
@@ -1,101 +1,78 @@
 /* eslint-disable */
-import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices'
-import { Observable } from 'rxjs'
+import { GrpcMethod, GrpcStreamMethod } from "@nestjs/microservices";
+import { Observable } from "rxjs";
 
-export const protobufPackage = 'auth'
+export const protobufPackage = "auth";
 
 /** proto/auth.proto */
 
 /** CcmsLogin */
 export interface CcmsLoginRequest {
-  accessToken: string
-  username?: string | undefined
+  accessToken: string;
+  username?: string | undefined;
 }
 
 export interface CcmsLoginResponse {
-  login: LoginResponse | undefined
-  campaigns: CampaignsResponse[] | undefined
+  login: LoginResponse | undefined;
+  campaigns: CampaignsResponse | undefined;
 }
 
 export interface LoginResponse {
-  idccms?: number | undefined
-  username?: string | undefined
-  name?: string | undefined
-  charge?: string | undefined
-  rol?: string | undefined
-  photo?: string | undefined
-  token?: string | undefined
+  idccms?: number | undefined;
+  username?: string | undefined;
+  name?: string | undefined;
+  charge?: string | undefined;
+  rol?: string | undefined;
+  photo?: string | undefined;
+  token?: string | undefined;
 }
 
 export interface CampaignsResponse {
-  id?: number | undefined
-  campaign?: string | undefined
-  powerBiName?: string | undefined
-  powerBiURL?: string | undefined
-  reportPowerBi?: string | undefined
+  id?: number | undefined;
+  campaign?: string | undefined;
+  powerBiName?: string | undefined;
+  powerBiURL?: string | undefined;
+  reportPowerBi?: string | undefined;
+  market?: string | undefined;
 }
 
 export interface ValidateTokenRequest {
-  token: string
+  token: string;
 }
 
 export interface ValidateTokenResponse {
-  auth: boolean
+  auth: boolean;
 }
 
-export const AUTH_PACKAGE_NAME = 'auth'
+export const AUTH_PACKAGE_NAME = "auth";
 
 export interface AuthServiceClient {
-  ccmsLogin(request: CcmsLoginRequest): Observable<CcmsLoginResponse>
+  ccmsLogin(request: CcmsLoginRequest): Observable<CcmsLoginResponse>;
 
-  validateToken(
-    request: ValidateTokenRequest,
-  ): Observable<ValidateTokenResponse>
+  validateToken(request: ValidateTokenRequest): Observable<ValidateTokenResponse>;
 }
 
 export interface AuthServiceController {
-  ccmsLogin(
-    request: CcmsLoginRequest,
-  ):
-    | Promise<CcmsLoginResponse>
-    | Observable<CcmsLoginResponse>
-    | CcmsLoginResponse
+  ccmsLogin(request: CcmsLoginRequest): Promise<CcmsLoginResponse> | Observable<CcmsLoginResponse> | CcmsLoginResponse;
 
   validateToken(
     request: ValidateTokenRequest,
-  ):
-    | Promise<ValidateTokenResponse>
-    | Observable<ValidateTokenResponse>
-    | ValidateTokenResponse
+  ): Promise<ValidateTokenResponse> | Observable<ValidateTokenResponse> | ValidateTokenResponse;
 }
 
 export function AuthServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ['ccmsLogin', 'validateToken']
+    const grpcMethods: string[] = ["ccmsLogin", "validateToken"];
     for (const method of grpcMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(
-        constructor.prototype,
-        method,
-      )
-      GrpcMethod('AuthService', method)(
-        constructor.prototype[method],
-        method,
-        descriptor,
-      )
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
     }
-    const grpcStreamMethods: string[] = []
+    const grpcStreamMethods: string[] = [];
     for (const method of grpcStreamMethods) {
-      const descriptor: any = Reflect.getOwnPropertyDescriptor(
-        constructor.prototype,
-        method,
-      )
-      GrpcStreamMethod('AuthService', method)(
-        constructor.prototype[method],
-        method,
-        descriptor,
-      )
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("AuthService", method)(constructor.prototype[method], method, descriptor);
     }
-  }
+  };
 }
 
-export const AUTH_SERVICE_NAME = 'AuthService'
+export const AUTH_SERVICE_NAME = "AuthService";
